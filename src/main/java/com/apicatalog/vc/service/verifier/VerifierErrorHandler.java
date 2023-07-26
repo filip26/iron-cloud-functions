e74bde77927d140384a37cacd185fe0c0dbb74f3
Filip <filip26@gmail.com>
2023-07-26 16:28:58 +0200

    Translate error codes

--- src/main/java/com/apicatalog/vc/service/verifier/VerifierErrorHandler.java
+++ src/main/java/com/apicatalog/vc/service/verifier/VerifierErrorHandler.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import com.apicatalog.ld.DocumentError;
 import com.apicatalog.ld.signature.VerificationError;
+import com.apicatalog.ld.signature.VerificationError.Code;
 import com.apicatalog.vc.service.Constants;
 
 import io.vertx.core.Handler;
@@ -25,13 +26,13 @@ class VerifierErrorHandler implements Handler<RoutingContext> {
 
         if (e instanceof VerificationError ve) {
 
-            verificationResult.addError(toString(ve.getCode().name()));
+            verificationResult.addCheck(toVcErrorCode(ve.getCode()));
 
             ctx.response().setStatusCode(400);
 
         } else if (e instanceof DocumentError de) {
 
-            verificationResult.addError("MALFORMED");
+            verificationResult.addError(toVcErrorCode(de.getCode()));
             verificationResult.addError(toString(de.getCode()));
 
             ctx.response().setStatusCode(400);
@@ -43,7 +44,6 @@ class VerifierErrorHandler implements Handler<RoutingContext> {
 
             ctx.response().setStatusCode(400);
 
-
         } else {
             e.printStackTrace();
             ctx.response().setStatusCode(500);
@@ -52,12 +52,42 @@ class VerifierErrorHandler implements Handler<RoutingContext> {
         var content = verificationResult.toString();
 
         ctx.response()
-            .putHeader("content-type", "application/json")
-            .putHeader("content-length", Integer.toString(content.getBytes().length))
-            .end(content);
+                .putHeader("content-type", "application/json")
+                .putHeader("content-length", Integer.toString(content.getBytes().length))
+                .end(content);
     }
 
     static String toString(String code) {
         return String.join("_", Arrays.stream(code.split("(?=\\p{Upper})")).map(String::toUpperCase).toList());
+    }
+
+    static String toVcErrorCode(String code) {
+        if ("MissingVerificationMethod".equals(code)
+                || "MissingProofVerificationId".equals(code)
+                || "UnknownProofVerificationId".equals(code)) {
+            return "MALFORMED_PROOF_ERROR";
+        }
+
+        if ("InvalidProofPurpose".equals(code)) {
+            return "MISMATCHED_PROOF_PURPOSE_ERROR";
+        }
+        
+        return "MALFORMED";
+    }
+
+    static String toVcErrorCode(Code code) {
+
+        switch (code) {
+        case UnsupportedCryptoSuite:
+            return "INVALID_PROOF_CONFIGURATION";
+        case Expired:
+            return "EXPIRED";
+        case InvalidSignature:
+            return "INVALID_SIGNATURE";
+        case NotValidYet:
+            return "NOT_VALID_YET";
+        }
+
+        return "UNKNOWN";
     }
 }
